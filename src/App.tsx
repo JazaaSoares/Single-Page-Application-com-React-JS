101c10ab18084adcb37d64d1d357a6f6afc88f3d
Jaza Soares <jazasoares03@gmail.com>
2025-02-09 23:46:41 -0300

    adicionando arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header/index.tsx';
 import MediaSocial from './components/MediaSocial/index.tsx';
+import SobreMim from './components/SobreMim/index.tsx';
 
 const App: React.FC = () => {
   return (
     <div id="all">
       <Header />
       <MediaSocial />
+      <SobreMim />
     </div>
   );
 };
